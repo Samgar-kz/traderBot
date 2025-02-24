94ac02875f6fbb0bb104b6cb0cb57419f58348a9
Samgar-kz <abdikozha.samgar@gmail.com>
2025-02-24 16:41:24 -0500

    grg

--- core/trading_logic.py
+++ core/trading_logic.py
@@ -145,14 +145,6 @@ async def send_market_report():
 
     formatted_data = format_historical_data(historical_data)
 
-    print("🔍 formatted_data:", formatted_data)
-
-    first_timestamp = formatted_data['BTC/USDT']['timestamps'][0]
-    last_timestamp = formatted_data['BTC/USDT']['timestamps'][-1]
-    print(f"⏳ Временной диапазон: {first_timestamp} - {last_timestamp}")
-
-    print(f"💰 Первые 5 цен: {formatted_data['BTC/USDT']['prices'][:5]}")
-
     await send_price_chart(formatted_data)  # Отправляем только если есть данные
 
 def format_historical_data(historical_data):
